1d4be864b3b05bcd209b19c6658fbd65ee474b7e
TUSHAR_iMRAN <tusharimran.pro@gmail.com>
2022-01-22 02:08:14 +0600

    Image grid fixed

--- src/components/Imagecard.js
+++ src/components/Imagecard.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageref = React.createRef();
+  }
+  componentDidMount() {
+    this.imageref.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageref.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageref} alt={description} src={urls.regular} />
       </div>
     );
   }
